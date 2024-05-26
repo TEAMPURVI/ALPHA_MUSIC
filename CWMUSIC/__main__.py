47cfb2d6598905c07e400d8583a8e8a2ae8f3eac
𝐀ʟᴘʜᴀ 𝐁ᴀʙʏ <168528642+TEAMPURVI@users.noreply.github.com>
2024-05-26 11:56:25 +0530

    Update __main__.py

--- CWMUSIC/__main__.py
+++ CWMUSIC/__main__.py
@@ -50,7 +50,7 @@ async def init():
         pass
     await Anony.decorators()
     LOGGER("CWMUSIC").info(
-        "DROP YOUR GIRLFRIEND'S NUMBER AND SEXY PIC TO @DEVIL_IZ_BACK , @ARYANSTUDYGROUP FOR ANY ISSUES"
+        "DROP YOUR GIRLFRIEND'S NUMBER AND SEXY PIC TO @YOUR_ALPHA_BABY , @PURVI_UPDATES FOR ANY ISSUES"
     )
     await idle()
     await app.stop()
